90bba5206362f7a515ab7350f65bbadff5ee8f82
Aleksey Kliger <alklig@microsoft.com>
2024-02-12 11:12:48 -0500

    really make it work on OSX

--- app/nativeaot.cmake
+++ app/nativeaot.cmake
@@ -7,19 +7,19 @@
 set(NAOT_SDK_BASE_BOOTSTRAP
     libbootstrapperdll.o)
 set(NAOT_SDK_BASE_LIBS
-  libRuntime.WorkstationGC.a
-  libeventpipe-disabled.a
-  libstdc++compat.a)
+  Runtime.WorkstationGC
+  eventpipe-disabled
+  stdc++compat)
 
 addprefix(NAOT_SDK_BOOTSTRAP "${NATIVEAOT_SDK_PATH}" "${NAOT_SDK_BASE_BOOTSTRAP}")
 addprefix(NAOT_SDK_LIBS "${NATIVEAOT_SDK_PATH}" "${NAOT_SDK_BASE_LIBS}")
 
 set(NAOT_FRAMEWORK_BASE_LIBS
-  libSystem.Native.a
-  libSystem.Globalization.Native.a
-  libSystem.IO.Compression.Native.a
-  libSystem.Net.Security.Native.a
-  libSystem.Security.Cryptography.Native.OpenSsl.a)
+  System.Native
+  System.Globalization.Native
+  System.IO.Compression.Native
+  System.Net.Security.Native
+  System.Security.Cryptography.Native.OpenSsl)
 
 addprefix(NAOT_FRAMEWORK_LIBS "${NATIVEAOT_FRAMEWORK_PATH}" "${NAOT_FRAMEWORK_BASE_LIBS}")
 
@@ -30,10 +30,11 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
   string(REPLACE ";" ":" NAOT_FRAMEWORK_EXCLUDE_ARG "${NAOT_FRAMEWORK_BASE_LIBS}")
   target_link_libraries (nativeAotFramework INTERFACE "${NAOT_SDK_BOOTSTRAP}" "${NAOT_SDK_LIBS}" "${NAOT_FRAMEWORK_LIBS}" -lm)
   target_link_options (nativeAotFramework INTERFACE "LINKER:--exclude-libs=${NAOT_SDK_EXCLUDE_ARG}:${NAOT_FRAMEWORK_EXCLUDE_ARG}")
+  target_link_options(nativeAotFramework INTERFACE "LINKER:--discard-all")
+  target_link_options(nativeAotFramework INTERFACE "LINKER:--gc-sections")
 elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
-  list(TRANSFORM NAOT_SDK_LIBS PREPEND "-hidden-l" OUTPUT_VARIABLE NAOT_SDK_HIDDEN_LIBS)
-  list(TRANSFORM NAOT_FRAMEWORK_LIBS PREPEND "-hidden-l" OUTPUT_VARIABLE NAOT_FRAMEWORK_HIDDEN_LIBS)
+  list(TRANSFORM NAOT_SDK_BASE_LIBS PREPEND "-Wl,-hidden-l" OUTPUT_VARIABLE NAOT_SDK_HIDDEN_LIBS)
+  list(TRANSFORM NAOT_FRAMEWORK_BASE_LIBS PREPEND "-Wl,-hidden-l" OUTPUT_VARIABLE NAOT_FRAMEWORK_HIDDEN_LIBS)
+  target_link_directories(nativeAotFramework INTERFACE "${NATIVEAOT_FRAMEWORK_PATH}" "${NATIVEAOT_SDK_PATH}")
   target_link_libraries(nativeAotFramework INTERFACE "${NAOT_SDK_BOOTSTRAP}" "${NAOT_SDK_HIDDEN_LIBS}" "${NAOT_FRAMEWORK_HIDDEN_LIBS}" -lm)
 endif()
-target_link_options(nativeAotFramework INTERFACE "LINKER:--discard-all")
-target_link_options(nativeAotFramework INTERFACE "LINKER:--gc-sections")
